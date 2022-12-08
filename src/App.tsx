ae8b511ecc1287b8b9798e08752b4fc4eeef9238
lucasquearis <lucasquearis@hotmail.com>
2022-12-08 12:05:31 -0300

    feat: adiciona task com id unicos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import ToDoList from "./components/TO-DO-List";
 import styled from "styled-components";
+import { useContext, useState } from "react";
+import { TaskContext } from "./context/TasksProvider";
+import { v4 } from "uuid";
 
 const HomeContainer = styled.div`
   max-width: 1440px;
@@ -35,14 +38,30 @@ const InputContainer = styled.div`
 `;
 
 function App() {
+  const { toDoLists, setToDoLists } = useContext(TaskContext);
+  const [taskName, setTaskName] = useState("");
+
+  const addTaskOnBoard = (taskName: String) => {
+    console.log({ taskName });
+    console.log({ toDoLists });
+    const newBoard = Array.from(toDoLists.board);
+    newBoard.push({ content: taskName, id: v4() });
+    setToDoLists((prevState) => ({ ...prevState, board: newBoard }));
+  };
+
   return (
     <HomeContainer>
       <TitleContainer>
         <h1>To-Do Semanal</h1>
         <p>Adicione uma tarefa ao board</p>
         <InputContainer>
-          <input placeholder="Dê um título a tarefa" />
-          <button>Enviar ao board</button>
+          <input
+            onChange={({ target: { value } }) => setTaskName(value)}
+            placeholder="Dê um título a tarefa"
+          />
+          <button onClick={() => addTaskOnBoard(taskName)}>
+            Enviar ao board
+          </button>
         </InputContainer>
       </TitleContainer>
       <ToDoList />
--- src/context/TasksProvider.tsx
+++ src/context/TasksProvider.tsx
@@ -4,6 +4,7 @@ import {
   useState,
   Dispatch,
   SetStateAction,
+  useEffect,
 } from "react";
 import { IToDoList } from "../types";
 
@@ -28,21 +29,17 @@ export const TaskContext = createContext(ResizeDefaultValue);
 
 export default function TasksProvider({ children }: { children: ReactNode }) {
   const [toDoLists, setToDoLists] = useState<IToDoList>({
-    board: [
-      { content: "example 1", id: `board-${1}` },
-      { content: "example 2", id: `board-${2}` },
-      { content: "example 3", id: `board-${3}` },
-    ],
-    monday: [
-      { content: "segunda 1", id: `monday-${1}` },
-      { content: "segunda 2", id: `monday-${2}` },
-      { content: "segunda 3", id: `monday-${3}` },
-    ],
-    tuesday: [{ content: "terça", id: `tuesday-${1}` }],
-    wednesday: [{ content: "quarta", id: `wednesday-${1}` }],
-    thursday: [{ content: "quinta", id: `thursday-${1}` }],
-    friday: [{ content: "sexta", id: `friday-${1}` }],
+    board: [],
+    monday: [],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: [],
   });
+
+  useEffect(() => {
+    console.log(toDoLists);
+  }, [toDoLists]);
 
   const value = { toDoLists, setToDoLists };
   return <TaskContext.Provider value={value}>{children}</TaskContext.Provider>;
